394bd8a908b84614bcbb39815f7292d61af5298d
biancapcr <biancacristina.pocora@gmail.com>
2025-07-07 16:25:34 +0200

    [add] generate computer random number ex-02

--- 02-Pari&Dispari/js/script.js
+++ 02-Pari&Dispari/js/script.js
@@ -3,3 +3,8 @@ let choice = prompt("Scegli 'pari' o 'dispari':");
 
 //Chiedere all'utente di digitare un num da 1 a 5
 let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+
+//Generare un num casuale per il computer con utilizzo di funzione
+function generateRandomNumber(){
+    return Math.floor(Math.random() * 5) + 1;
+}
