d029099638368c9b22ca49bf2af7c28bb7d80ac1
biancapcr <biancacristina.pocora@gmail.com>
2025-07-07 16:22:24 +0200

    [add] second prompt ex-02

--- 02-Pari&Dispari/js/script.js
+++ 02-Pari&Dispari/js/script.js
@@ -1,2 +1,5 @@
 // Chiedere all'utente di digitare un num pari o dispari
 let choice = prompt("Scegli 'pari' o 'dispari':");
+
+//Chiedere all'utente di digitare un num da 1 a 5
+let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
